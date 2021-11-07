f906624c0054c1101b6a5855962f1a7ddd67b482
Bashful <Christophereek97@gmail.com>
2021-11-07 08:57:43 +0100

    First Commit

---
+++ dev_mode.py
@@ -0,0 +1,102 @@
+import json
+import sys
+from typing import Dict, List
+from typing import NewType
+
+pass_ob = []
+
+
+class PassVerify:
+    
+    def __init__(self, id_main: str, id_hash: str, username: str, password: str, name: str) -> object:
+
+        self.id_main: str= id_main
+        self.id_hash: str = id_hash
+        self.user: str = username
+        self.passw: str = password
+        self.name: str = name
+        self.ref: List[str] = [self.user, self.name, self.passw]
+
+            
+def list_dups(struct_list):
+    
+    sus_dupes = []
+    dupes = []
+    
+    listele = len(struct_list) - 1
+    
+    for jndex, obj in enumerate(struct_list):
+        if jndex < listele:
+            for obj2 in struct_list[int(jndex + 1):]: 
+                #print(obj.id_main[1])
+                if str(obj.ref[0]).lower() in str(obj2.ref[0]).lower():
+                    #print("first")
+                    if str(obj.ref[1]) in str(obj2.ref[1]):
+                        #rint("secound")
+                        most = [obj, obj2]
+                        sus_dupes.append(most)
+                        break
+    
+    for j, i in enumerate(sus_dupes):
+        x1 = sys.getsizeof(sus_dupes[j][0])
+        x2 = sys.getsizeof(sus_dupes[j][1])
+        p1 = sus_dupes[j][0].passw
+        p2 = sus_dupes[j][1].passw
+
+    
+
+        if x1 >= x2 or p1 >= p2:
+            dupes.append(sus_dupes[j][1].id_main)
+        elif x2 > x1 and p2 > p1:
+            dupes.append(sus_dupes[j][0].id_main)
+
+    return dupes
+            
+                    
+                    
+        
+             
+def main():
+    v1 = "default"
+    j1 = []
+
+    
+    
+    with open('bitwarden_export.json') as f:
+        print(f)
+        data = json.load(f)
+        v1 = data
+        for j, i in enumerate(data["items"]):
+            try:
+                id_obj = j
+                user_obj = data["items"][j]["login"]["username"]
+                pass_obj = data["items"][j]["login"]["password"]
+                id_hash = data["items"][j]["id"]
+                name_obj = data["items"][j]["name"]
+                pass_ob.append(PassVerify(id_obj, id_hash, user_obj, pass_obj, name_obj))
+            except: True
+            
+        dupes = list_dups(pass_ob)
+
+        with open('update_export.json', "r+") as f2:
+            data2 = json.load(f2)
+            count = 0
+            #print(data2)
+            for j, i in enumerate(v1["items"]):
+                print(i)
+                try:
+                    #print(i)
+                    if j in dupes:
+                        a = 13
+                    else:
+                       data2["items"].append(v1["items"][j])
+                except: pass
+            data2.update()
+            f2.seek(0)
+            json.dump(data2, f2)
+
+            
+            
+            
+if __name__  == "__main__":
+    main()
---
+++ json_alter.go
@@ -0,0 +1,86 @@
+package main
+
+import (
+	`encoding/json`
+	`fmt`
+	`io/ioutil`
+	`os`
+)
+
+type Heads struct{
+	Heads 			[]Items	`json:"items"`
+}
+
+type Login struct {
+	Username		string	`json:"username"`
+	Password		string	`json:"Password"`
+	Totp			string	`json:"totp"`
+}
+
+type Items struct {
+	Id				string 	`json:"id"`
+	OrganizationId	string 	`json:"OrganizationId"`
+	FolderId		string 	`json:"folderId"`
+	Reprompt		int 	`json:"reprompt"`
+	Name			string 	`json:"name"`
+	Notes			string	`json:"notes"`
+	Favorite		string 	`json:"favorite"`
+	Login 			Login	`json:"login"`
+	Username		string	`json:"username"`
+
+}
+
+
+
+func main() {
+
+	jsonFile, err := os.Open("bitwarden_export.json")
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println("Successfully Opened json file")
+	defer jsonFile.Close()
+
+	byteEmpValue, _ := ioutil.ReadAll(jsonFile)
+
+	var heads Heads
+
+	json.Unmarshal(byteEmpValue, &heads)
+
+
+	for i := 0; i < len(heads.Heads); i++ {
+		fmt.Printf("Employee Name: " + heads.Heads[i].Login.Password)
+		//fmt.Println("Employee Gender: " + employees.Employees[i].Gender)
+		//fmt.Println("Employee Age: " + strconv.Itoa(employees.Employees[i].Age))
+	}
+
+
+
+
+
+
+
+
+
+
+	/*
+	// Open our jsonFile
+	jsonFile, err := os.Open("bitwarden_export.json")
+	// if we os.Open returns an error then handle it
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println("Successfully Opened users.json")
+	// defer the closing of our jsonFile so that we can parse it later on
+	defer jsonFile.Close()
+
+	byteValue, _ := ioutil.ReadAll(jsonFile)
+
+	var result interface{}
+	json.Unmarshal([]byte(byteValue), &result)
+
+	fmt.Println(result.(map[string]interface{})["items"].string["login"])*/
+
+	}
+
