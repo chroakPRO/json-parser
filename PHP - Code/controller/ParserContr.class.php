848d365b59e0275ef4ca9aee091081906a699923
Bashful <33976926+Bazhful@users.noreply.github.com>
2022-01-06 08:48:32 +0100

    k

--- PHP - Code/controller/ParserContr.class.php
+++ PHP - Code/controller/ParserContr.class.php
@@ -3,18 +3,34 @@ Namespace Controllers;
 
 class Parser { 
     
-    public function __construct(string $json_struct){
-        
+    // PRIVATE DATA
+    private object $json_decoded;
+
+    // PUBLIC DATA
+    public object $test;
+    
+    /**
+     * @param string $loaded_file
+     */
+    public function __construct(string $loaded_file){
+
         $this->db = new \Database\DB;
-        $this->json_struct =  $json_struct;
+        $this->json_struct =  $loaded_file;
         $this->linked_list = new \SplDoublyLinkedList();
     }
     
     
     
-    public function crawler(){
+    /**
+     * @global ['PHP Object Generator' - (account details)]
+     * 
+     * @param string $json_struct 
+     * @return \SplDoublyLinkedList
+     */
+    public function objGenerator(string $json_struct): \SplDoublyLinkedList {
         
-        $json_decoded = json_decode($this->json_struct);
+        // 
+        $json_decoded = json_decode($json_struct);
         for($i = 0; $i < count($this->json_decoded["items"]); $i++){
             
             $this->id_main = $i;
@@ -33,7 +49,32 @@ class Parser {
             $list_object->push($str_object);
             
         }
+
+        return $list_object;
+       
+    } 
+
+    /**
+     * @global ['returns list with duplicates']
+     * 
+     * @param array $arr
+     * // returns object
+     * @return array $seralizedObj
+     */
+    function duplicateNumber(array $arr): array {
         
+        for($i = 0; $i < count($arr); $i++){
+            for($j = 0; $j < $count($arr); $j++){
+                if (strpos($arr[i], $arr[$j + 1]) === false) {
+                    
+                
+                }
+            }
+        }
+        
+        
+
+        return item;
     }
 }
 ?>
---
+++ PHP - Code/model/AccountInfo.model.php
@@ -0,0 +1,89 @@
+<?php
+/**
+ * [Description]
+ * Every JSON object will be parsed and placed into an AccountDetail object
+ * 
+ * @version ${1:1.0.0
+ * @author Cooper <coopersec@protonmail.com>
+ * 
+ */
+ class AccountDetail  {
+
+    // PUBLIC DATA
+    public string $id_main, $id_hash, $username, $password, $name;
+
+    // PUBLIC DATA
+    public array $refrence;
+
+    /**
+     * @global ['Constructor']
+     */
+    public function __construct() {
+        
+        // Default values. 
+        $this->id_main = 0;
+        $this->id_hash = 0;
+        $this->username = "none";
+        $this->password = "none";
+        $this->name = "none";
+
+        // Array: Hold all object values. (not sorted)
+        $this->reference = array($this->username, $this->id_hash,
+         $this->username, $this->password, $this->name);
+    }
+
+    /**
+     * @global ['When object gets printed.']
+     * @return [string]
+     */
+    public function __toString() {
+        // String: Can be used to return values as strings. Without converting
+        return "struct-object blueprint. Nothing more to see here....";
+    }
+
+    /**
+     * @global ['when object gets invoked']
+     * @return [string]
+     */
+    public function _invoke() {
+        return 'Invoke is not allowed.';
+    }
+
+    /**
+     * @global ['function call:: seralize()']
+     * @return array
+     * Info: This is for testing purposes.
+     */
+    public function __seralize(): array 
+    {
+        
+       // JSON Structure: No info needed..
+        return [
+            'id'       => $this->id_main,
+            'hash'     => $this->id_hash,
+            'username' => $this->username,
+            'password' => $this->password,
+            'name'     => $this->name,
+            'ref'      => $this->reference,
+        ];
+
+    }
+
+    /**
+     * @global ['function call: unserialize()']
+     * 
+     * @param array $data
+     * Info: For testing purposes.
+     * @return void
+     */
+    public function __unserialize(array $data): void
+    {
+        $this->id_main = $data['id_main'];
+        $this->id_hash = $data['hash'];
+        $this->username = $data['username'];
+        $this->password = $data['password'];
+        $this->name = $data['name'];
+        $this->reference = $data['reference'];
+    }
+}
+?>
